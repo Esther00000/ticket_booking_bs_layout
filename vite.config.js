65ce880cd623a407ea1cd2a352d9fb5e37ee80c1
Esther <fuest8456258@gmail.com>
2024-03-26 10:47:54 +0800

    first

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,17 @@ import { resolve } from "path";
 export default defineConfig({
   base: "/ticket_booking_bs_layout/",
   build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        activity: resolve(__dirname, "pages/activity.html"),
+        detail: resolve(__dirname, "pages/detail.html"),
+        login: resolve(__dirname, "pages/login.html"),
+        payment: resolve(__dirname, "pages/payment.html"),
+        payment02: resolve(__dirname, "pages/payment02.html"),
+        payment03: resolve(__dirname, "pages/payment03.html"),
+      },
+    },
     outDir: resolve(__dirname, "dist"),
   },
   server: {
